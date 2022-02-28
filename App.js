7a56a266a38c9b6c287c455754243acd2285f133
Davood Nikoo <davood33.x33@gmail.com>
2022-02-28 12:32:28 +0330

    getting location

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  useEffect(() => {
+    load();
+  }, []);
+  async function load() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMessage("Access to location is needed to run this app");
+      return;
+    }
+    const location = await Location.getCurrentPositionAsync();
+    const { latitude, longitude } = location.coords;
+    setLatitude(latitude);
+    setLongitude(longitude);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to bingo</Text>
+      <Text>latitude is = {latitude}</Text>
+      <Text>longitude is = {longitude}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +31,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderStyle: "solid",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "cyan",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
