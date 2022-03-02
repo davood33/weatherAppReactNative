89f5aadce48d6c3b88e330e51b42b1ff54936778
Davood Nikoo <davood33.x33@gmail.com>
2022-03-02 15:05:10 +0330

    weather info component created

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Location from "expo-location";
+import WeatherInfo from "./components/WeatherInfo";
 const WEATHER_API_KEY = "e351360858f6d3bb28f204f977f68575";
 const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
 export default function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitSystem, setUnitSystem] = useState("metric");
   useEffect(() => {
     load();
   }, []);
@@ -21,33 +23,38 @@ export default function App() {
 
       const { latitude, longitude } = location.coords;
 
-      const weatherUrl = `${WEATHER_BASE_URL}lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`;
+      const weatherUrl = `${WEATHER_BASE_URL}lat=${latitude}&lon=${longitude}&units=${unitSystem}&appid=${WEATHER_API_KEY}`;
 
       const response = await fetch(weatherUrl);
       const result = await response.json();
-      if(response.ok){
+      if (response.ok) {
         setCurrentWeather(result);
-      }
-      else{
-      setErrorMessage(result.message)
+      } else {
+        setErrorMessage(result.message);
       }
     } catch (error) {
       setErrorMessage(error.message);
     }
   }
   if (currentWeather) {
-    const {main : {temp}} = currentWeather
+    const {
+      main: { temp },
+    } = currentWeather;
     return (
       <View style={styles.container}>
-        <Text>{temp}</Text>
-        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <WeatherInfo currentWeather={currentWeather}/>
+          <StatusBar style="auto" />
+        </View>
       </View>
     );
   } else {
     return (
       <View style={styles.container}>
-        <Text>{errorMessage}</Text>
-        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <Text>{errorMessage}</Text>
+          <StatusBar style="auto" />
+        </View>
       </View>
     );
   }
@@ -55,12 +62,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    borderColor: "black",
-    borderWidth: 1,
-    borderStyle: "solid",
     flex: 1,
-    backgroundColor: "cyan",
-    alignItems: "center",
     justifyContent: "center",
+  },
+  main: {
+    flex: 1,
+    alignItems: "center",
   },
 });
---
+++ components/WeatherInfo.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+import { Text } from "react-native";
+import { colors } from "../utils/index";
+const { PRIMARY_COLOR, SECONDARY_COLOR } = colors;
+const WeatherInfo = ({ currentWeather }) => {
+  const {
+    main: { temp },
+    weather: [detail],
+    name,
+  } = currentWeather;
+  const { main, description, icon } = detail;
+  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  return (
+    <View style={styles.weatherInfo}>
+      <Text style={styles.name}>{name}</Text>
+      <Image source={{ uri: iconUrl }} style={styles.icon} />
+      <Text style={styles.textPrimary}>{temp}°</Text>
+      <Text style={styles.weatherDescription}>{description}</Text>
+      <Text style={styles.textSecondary}>{main}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  weatherInfo: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  icon: {
+    width: 100,
+    height: 100,
+  },
+  weatherDescription: {
+    textTransform:'capitalize',
+    color:'black',
+    fontSize: 17,
+    marginTop: 10
+  },
+  textPrimary: {
+    fontSize: 40,
+    color: PRIMARY_COLOR,
+  },
+  textSecondary: {
+    fontSize: 25,
+    color: SECONDARY_COLOR,
+    fontWeight : "500",
+    marginTop:10
+  },
+});
+
+export default WeatherInfo;
